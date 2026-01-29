3d53ffc313864530fb4ef0ed2aca9c47c96434ed
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-01-29 21:25:43 +0530

    chore: add a way to switch between peripherals on the spi bus

---
+++ registrator/spi_bus.h
@@ -0,0 +1,25 @@
+#ifndef SPI_BUS_H
+#define SPI_BUS_H
+
+#include <cstdint>
+#include <vector>
+
+
+std::vector<uint32_t> spi_ss_pins{};
+
+void enable_peripheral(uin32_t pin) {
+  for(const auto& spi_pin : spi_ss_pins) {
+    if (spi_pin == pin) {
+      digitalWrite(pin, HIGH);
+    }
+    digitalWrite(pin, LOW);
+  }
+}
+
+void add_peripheral(uint32_t pin) {
+  spi_ss_pins.push_back(pin);
+}
+
+
+
+#endif SPI_BUS_H
