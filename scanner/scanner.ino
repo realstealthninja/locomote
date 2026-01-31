0e22e66d6c220a5dbb3c9099fde610c49580a8ab
mahajandhruv578-afk <mahajandhruv578@gmail.com>
2026-01-31 17:08:35 +0530

    feat: add preliminary scanning code

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -1,11 +1,58 @@
-#include "scan.h"
+#include <SPI.h>
+#include <MFRC522v2.h>
+#include <MFRC522DriverSPI.h>
+#include <MFRC522DriverPinSimple.h>
 
-void setup()
-{
-  // put your setup code here, to run once:
+// Only SS pin, no RST connected
+#define SS_PIN D8   // GPIO15
+
+MFRC522DriverPinSimple ss_pin(SS_PIN);
+MFRC522DriverSPI driver(ss_pin);
+MFRC522 rfid(driver);
+
+void setup() {
+  Serial.begin(115200);  // high baud for faster debug
+  delay(1000);
+
+  Serial.println("==== ESP8266 + RC522 DEBUG ====");
+  Serial.print("SPI Init... ");
+  SPI.begin();
+  Serial.println("DONE");
+
+  Serial.print("RFID Init... ");
+  rfid.PCD_Init();
+  Serial.println("DONE");
+
+  Serial.println("Place a card near the reader...");
 }
 
-void loop()
-{
-  // put your main code here, to run repeatedly:
+void loop() {
+  // Check if card is present
+  if (!rfid.PICC_IsNewCardPresent()) {
+    Serial.println("No card detected");
+    delay(2000);
+    return;
+  }
+
+  Serial.println("Card detected!");
+
+  // Try reading UID
+  if (!rfid.PICC_ReadCardSerial()) {
+    Serial.println("Failed to read UID");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("UID: ");
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    Serial.print(rfid.uid.uidByte[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+  delay(2000);
 }
+
