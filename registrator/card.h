3957925cf05b802a5a5ac167175e47655f39ce29
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-05 08:20:27 +0530

    feat: create card class

--- registrator/card.h
+++ registrator/card.h
@@ -1,9 +1,29 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include <vector>
+#include <MFRC522v2.h>
+
+
 class Card {
 private:
-  std::vector<byte> uid;
+  std::vector<std::byte> uid;
+
+public:
+  Card() {
+
+  };
+
+  void write_data(const std::vector<std::byte>& bytes) {
+
+  }
+
+  void read_data(std::vector<std::byte>& bytes) {
+
+  }
+
+
+
 };
 
 
