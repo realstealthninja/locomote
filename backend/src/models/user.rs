086f469ea101112ffe0e11b98b6423d600447d64
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-06 05:48:59 +0530

    feat: fix user model

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -1,11 +1,11 @@
 use diesel::{Selectable, prelude::Queryable};
 
-
 #[derive(Queryable, Selectable)]
-#[diesel(table_name= crate::schema::user)]
+#[diesel(table_name=crate::schema::user)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
     id: i32,
-    user_id: i32,
+    user_id: String,
     username: String,
-    creation: String
+    creation: i32,
 }
