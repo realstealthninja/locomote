2c834481c391327c5a14c7bbb023211c4f0f7180
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-08 19:08:05 +0530

    feat: redo entire schema

--- backend/migrations/locomote/up.sql
+++ backend/migrations/locomote/up.sql
@@ -1,42 +1,76 @@
-CREATE TABLE "card"(
+CREATE TABLE "user"(
 	"id" SERIAL NOT NULL PRIMARY KEY,
-	"card_id" INT4 NOT NULL,
-	"user_id" INT4 NOT NULL,
-	"disabled" BOOLEAN NOT NULL,
-	"created_at" TIMESTAMP NOT NULL,
-	"updated_at" TIMESTAMP NOT NULL,
-	"balance" FLOAT4 NOT NULL
+	"username" VARCHAR NOT NULL,
+	"created_at" TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
+
+CREATE TRIGGER mdt_moddatetime
+    BEFORE UPDATE ON "user"
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (updated_at);
 
 CREATE TABLE "useraccount"(
 	"id" SERIAL NOT NULL PRIMARY KEY,
-	"user_id" INT4 NOT NULL,
+	"user_id" INT4 NOT NULL REFERENCES "user"(id),
 	"username" VARCHAR NOT NULL,
-	"password_hash" VARCHAR NOT NULL
+	"password_hash" VARCHAR NOT NULL,
+	"created_at" TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
+
+CREATE TRIGGER mdt_moddatetime
+    BEFORE UPDATE ON "useraccount"
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (updated_at);
+
+
+
+
+CREATE TABLE "card"(
+	"id" SERIAL NOT NULL PRIMARY KEY,
+	"user_id" INT4 NOT NULL REFERENCES "user"(id),
+	"card_id" VARCHAR NOT NULL,
+	"disabled" BOOLEAN NOT NULL DEFAULT FALSE,
+	"created_at" TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	"balance" FLOAT4 NOT NULL DEFAULT 0.0
+);
+
+CREATE TRIGGER mdt_moddatetime
+    BEFORE UPDATE ON "card"
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (updated_at);
+
+
 
 CREATE TABLE "scanner"(
 	"id" SERIAL NOT NULL PRIMARY KEY,
-	"scanner_id" INT4 NOT NULL
+	"created_at" TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
-CREATE TABLE "user"(
-	"id" SERIAL NOT NULL PRIMARY KEY,
-	"user_id" VARCHAR NOT NULL,
-	"username" VARCHAR NOT NULL,
-	"created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL
-);
+CREATE TRIGGER mdt_moddatetime
+    BEFORE UPDATE ON "scanner"
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (updated_at);
+
+
 
 CREATE TABLE "ticket"(
 	"id" SERIAL NOT NULL PRIMARY KEY,
-	"card_id" INT4 NOT NULL,
-	"user_id" INT4 NOT NULL,
-	"origin" VARCHAR NOT NULL,
-	"destination" VARCHAR NOT NULL,
-	"validity" TIMESTAMP NOT NULL,
-	"travelling_at" TIMESTAMP NOT NULL,
-    "created_at" TIMESTAMP NOT NULL,
+	"card_id" INT4 NOT NULL REFERENCES card(id),
+	"user_id" INT4 NOT NULL REFERENCES "user"(id),
+	"origin" VARCHAR,
+	"destination" VARCHAR,
+	"validity" TIMESTAMP WITH TIME ZONE,
+	"travelling_at" TIMESTAMP WITH TIME ZONE,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT now(),
+	"updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now(),
 	"cost" FLOAT4 NOT NULL
 );
 
+CREATE TRIGGER mdt_moddatetime
+    BEFORE UPDATE ON "ticket"
+    FOR EACH ROW
+    EXECUTE PROCEDURE moddatetime (updated_at);
