57768bb158a32afda20129216029b4c032dcd3d6
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-12 05:26:06 +0530

    feat: add card model

--- backend/src/models/card.rs
+++ backend/src/models/card.rs
@@ -1,13 +1,40 @@
-use diesel::{Selectable, prelude::Queryable,};
+use diesel::{RunQueryDsl, Selectable, SelectableHelper, prelude::{Associations, Identifiable, Insertable, Queryable}};
+use serde::Deserialize;
+use time::PrimitiveDateTime;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name= crate::schema::card)]
+use crate::{models::user::User, schema::card};
+
+#[derive(Queryable, Selectable, Identifiable, Associations)]
+#[diesel(table_name= card)]
 #[diesel(belongs_to(User))]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Card {
     pub id: i32,
-    pub card_id: i32,
     pub user_id: i32,
     pub disabled: bool,
-    pub balance: f32
+    pub card_id: String,
+    pub balance: f32,
+    pub created_at: Option<PrimitiveDateTime>,
+    pub updated_at: Option<PrimitiveDateTime>
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name = card)]
+pub struct NewCard {
+    pub user_id: i32,
+    pub card_id: String,
+}
+
+pub async fn create_card(pool: &deadpool_diesel::postgres::Pool, card: NewCard) -> Card {
+    let conn = pool.get().await.expect("Cant get connection to pool");
+
+    conn.interact(|conn| {
+        let new_card = card;
+        diesel::insert_into(card::table)
+        .values(&new_card)
+        .returning(Card::as_returning())
+        .get_result(conn)
+        .expect("Card couldnt be inserted")
+        }
+    ).await.expect("Card couldnt be inserted")
 }
