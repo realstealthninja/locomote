60fdf4f8301786e5433b537d0e027e284c3d9752
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-12 05:26:54 +0530

    feat: redo user model

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -1,13 +1,40 @@
-use diesel::{Selectable, prelude::Queryable};
+use diesel::{
+    RunQueryDsl, Selectable, SelectableHelper,
+    prelude::{Identifiable, Insertable, Queryable},
+};
+use serde::{Deserialize, Serialize};
 use time::PrimitiveDateTime;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name=crate::schema::user)]
+use crate::schema::user;
+
+#[derive(Queryable, Selectable, Identifiable)]
+#[diesel(table_name = user)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
     pub id: i32,
-    pub user_id: String,
     pub username: String,
-    pub created_at: PrimitiveDateTime,
-    pub updated_at:PrimitiveDateTime
+    pub created_at: Option<PrimitiveDateTime>,
+    pub updated_at: Option<PrimitiveDateTime>,
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name = user)]
+pub struct NewUser {
+    pub username: String,
+}
+
+pub async fn create_user(pool: &deadpool_diesel::postgres::Pool, user: NewUser) -> User {
+    
+
+    let conn = pool.get().await.expect("Cant get connection to pool");
+
+    conn.interact(|conn| {
+        let new_user = user;
+        diesel::insert_into(user::table)
+            .values(&new_user)
+            .returning(User::as_returning())
+            .get_result(conn)
+            .expect("Error saving new user")
+        }
+    ).await.expect("User couldnt be inserted")
 }
