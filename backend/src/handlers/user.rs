120f5b27716398ee7d5f3e3e329cc72aa347b780
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-12 05:24:09 +0530

    feat: define post user

--- backend/src/handlers/user.rs
+++ backend/src/handlers/user.rs
@@ -1,11 +1,29 @@
-fn create_user() {
+use crate::models::user::{NewUser, create_user};
+use crate::state::AppState;
+use axum::{Json, extract::{self, State}};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct UserResponse {
+    id: i32,
+    username: String,
 }
 
 
-fn delete_user() {
+pub async fn post_user(
+    State(state): State<AppState>,
+    extract::Json(new_user): extract::Json<NewUser>,
+) -> extract::Json<UserResponse>
+{
+    println!("POST: USER");
+    let user = create_user(&state.pool, new_user).await;
 
+    Json(UserResponse {
+        id: user.id,
+        username: user.username
+    })
 }
 
-fn update_user() {
+pub async fn delete_user() {}
 
-}
+pub async fn update_user() {}
