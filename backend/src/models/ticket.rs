4304e932e057d65dbeb19275d4823b5fefabe590
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-12 05:26:41 +0530

    fix: add created and updated to model

--- backend/src/models/ticket.rs
+++ backend/src/models/ticket.rs
@@ -1,18 +1,23 @@
-use diesel::{Selectable, prelude::Queryable};
+use diesel::{Selectable, prelude::{Associations, Identifiable, Queryable}};
 use time::{Date, PrimitiveDateTime, Time};
 
+use crate::models::{card::Card, user::User};
 
-#[derive(Queryable, Selectable)]
+
+#[derive(Queryable, Selectable, Identifiable, Associations)]
 #[diesel(table_name= crate::schema::ticket)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 #[diesel(belongs_to(User))]
+#[diesel(belongs_to(Card))]
 pub struct Ticket {
     pub id: i32,
     pub card_id: i32,
     pub user_id: i32,
-    pub origin: String,
-    pub destination: String,
-    pub validity: PrimitiveDateTime,
-    pub travelling_at: PrimitiveDateTime,
+    pub origin: Option<String>,
+    pub destination: Option<String>,
+    pub validity: Option<PrimitiveDateTime>,
+    pub travelling_at: Option<PrimitiveDateTime>,
+    pub created_at: Option<PrimitiveDateTime>,
+    pub updated_at: Option<PrimitiveDateTime>,
     pub cost: f32
 }
