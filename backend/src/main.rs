c807f73ab1b1f6ad713d4a828ee73dc6f0de4148
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-12 05:24:40 +0530

    feat: use pool and defer router to a different file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,40 +1,35 @@
 mod schema;
 mod models;
+mod routes;
+mod state;
+mod handlers;
 
 use std::env;
-
-use diesel::prelude::*;
-use axum::{
-    routing::{get},
-    Router
-};
-
+use deadpool_diesel::{Manager, Pool, Runtime};
 use dotenvy::dotenv;
 
-fn db_connect() -> PgConnection {
-    dotenv().ok();
+use crate::{routes::router, state::AppState};
 
-    let url = env::var("DATABASE_URL").expect("Database url in env must be set");
-    PgConnection::establish(&url).unwrap_or_else(|_| panic!("Error connecting to {url}"))
-}
 
 #[tokio::main]  
 async fn main() -> Result<(), std::io::Error>{
     tracing_subscriber::fmt::init();
     
-    db_connect();
+    dotenv().ok();
 
-    let app = Router::new()
-     .route("/", get(root));
+    let url = env::var("DATABASE_URL").expect("Database url in env must be set");
+    let manager = Manager::new(url.to_string(), Runtime::Tokio1);
 
+    let pool = Pool::builder(manager)
+        .build()
+        .expect("Failed to create pool");
 
-    
+    let app_state = AppState{ pool };
+    let router = router(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0:8080").await.unwrap();
-    axum::serve(listener, app).await
+    
+    println!("Lisenting on port 8080");
+    axum::serve(listener, router).await
 }
 
-
-async fn root() -> &'static str {
-    "Hello world"
-}
