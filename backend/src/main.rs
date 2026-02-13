34d64477b5673ff21a7f30642fdc0ad16754eff9
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-13 17:51:31 +0530

    feat: use sqlx

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,30 +1,25 @@
-mod schema;
-mod models;
 mod routes;
-mod state;
 mod handlers;
+mod state;
 
 use std::env;
-use deadpool_diesel::{Manager, Pool, Runtime};
 use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 use crate::{routes::router, state::AppState};
 
 
 #[tokio::main]  
 async fn main() -> Result<(), std::io::Error>{
-    tracing_subscriber::fmt::init();
-    
     dotenv().ok();
 
-    let url = env::var("DATABASE_URL").expect("Database url in env must be set");
-    let manager = Manager::new(url.to_string(), Runtime::Tokio1);
+    let db_url = env::var("DATABASE_URL")
+                            .expect("Database url in env must be set");
 
-    let pool = Pool::builder(manager)
-        .build()
-        .expect("Failed to create pool");
+    let pool = PgPoolOptions::new().connect(&db_url).await
+                            .expect("Couldnt connect to db");
 
-    let app_state = AppState{ pool };
+    let app_state = AppState{pool};
     let router = router(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0:8080").await.unwrap();
