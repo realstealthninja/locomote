ed612e3788ddffa2532b6feb5a3ae0334d41146b
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-21 16:41:05 +0530

    feat: add routes to router

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,12 +1,14 @@
 use axum::{Router, routing::{get, post}};
 
-use crate::{AppState, handlers::user, handlers::card};
+use crate::{AppState, handlers::{card, scanner, user}};
 
 
 pub fn router(state: AppState) -> Router {
     Router::new()
         .route("/", get(root))
+        .route("/v1/verify", post(scanner::post_verify))
         .nest("/v1/users", user_router(state.clone()))
+        .nest("/v1/cards", card_router(state.clone()))
         .with_state(state)
 }
 
@@ -20,7 +22,10 @@ pub fn card_router(state: AppState) -> Router<AppState> {
     Router::new()
         .route("/", post(card::post_card))
         .with_state(state)
+        
 }
+
+
 
 async fn root() -> &'static str {
     "Ok"
