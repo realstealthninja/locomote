b7d76233d283bccc7ad641cba04bb8104bdf9f97
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-21 16:40:08 +0530

    feat: finish card create endpoint

--- backend/src/handlers/card.rs
+++ backend/src/handlers/card.rs
@@ -1,33 +1,37 @@
-use axum::extract::{self, State};
-use serde::Serialize;
+use axum::{
+    Json,
+    extract::{State},
+};
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
 
-use crate::{models::card::{NewCard, create_card}, state::AppState};
+use crate::state::AppState;
 
-#[derive(Serialize)]
-pub struct CardResponse {
-    pub id: i32,
-    pub card_id: String,
+#[derive(Deserialize, Debug)]
+pub struct CardRequest {
+    pub code: String,
     pub user_id: i32,
-    pub balance: f32
 }
 
+#[derive(Serialize, FromRow, Debug)]
+pub struct CardResponse {
+    pub id: i32,
+    pub user_id: i32,
+}
 
 pub async fn post_card(
     State(state): State<AppState>,
-    extract::Json(new_card): extract::Json<NewCard>,
-) ->  extract::Json<CardResponse> {
-    let card = create_card(&state.pool, new_card).await;
-
-    extract::Json(CardResponse{
-        id: card.id,
-        card_id: card.card_id,
-        user_id: card.user_id,
-        balance: card.balance
-    })
+    Json(card): Json<CardRequest>,
+) -> Json<CardResponse> {
+    Json(
+        sqlx::query_as::<_, CardResponse>(
+            "INSERT INTO card (code, user_id) VALUES ($1, $2) RETURNING id, user_id",
+        )
+        .bind(card.code)
+        .bind(card.user_id)
+        .fetch_one(&state.pool)
+        .await
+        .expect("unable to insert card into database"),
+    )
 }
-
-pub async fn get_card(
-    State(state): State<AppState>
-) {
-
-}
+    
